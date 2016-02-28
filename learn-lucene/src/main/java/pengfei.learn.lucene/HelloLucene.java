a310b3a26505bdff450a0062a154c8ee57bef13c
pengfeis <supengfei007@gmail.com>
2016-02-28 17:18:10 +0800

    lucene

--- learn-lucene/src/main/java/pengfei.learn.lucene/HelloLucene.java
+++ learn-lucene/src/main/java/pengfei.learn.lucene/HelloLucene.java
@@ -1,4 +1,30 @@
 package pengfei.learn.lucene;
 
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import java.io.File;
+
 public class HelloLucene {
+
+    public void createIndex() throws IOException {
+        Directory directory = FSDirectory.open(Paths.get("./foo"));
+        Analyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+        iwc.setOpenMode(OpenMode.CREATE);
+
+        IndexWriter indexWriter = new IndexWriter(directory, iwc);
+
+    }
+
+
+
 }
