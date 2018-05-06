8d18af0b986f74a9e6eca58b85241cf8e71deafa
pengfeis <supengfei007@gmail.com>
2018-05-06 12:15:33 +0800

    add jhm and lombook and histrix dependency

---
+++ x-frame-work/src/test/java/pengfei/learn/hystrix/ServiceHystrixCommandTest.java
@@ -0,0 +1,13 @@
+package pengfei.learn.hystrix;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ServiceHystrixCommandTest {
+    @Test
+    public void run() throws Exception {
+
+    }
+
+}
